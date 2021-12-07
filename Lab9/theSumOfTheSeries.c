edccc7a92aa52375ed7bba1d9553095092ab8ec7
BeratBilgic <bertbilgc@gmail.com>
2021-12-07 21:08:19 +0300

    commit

--- Lab9/theSumOfTheSeries.c
+++ Lab9/theSumOfTheSeries.c
@@ -4,7 +4,9 @@ int main()
 {
     float x,n,p;
     float fac = 1, sum = 1;
+    printf("Enter the value of x\n");
     scanf("%f",&x);
+    printf("Enter the value of n\n");
     scanf("%f",&n);
     
     for (float i = 1; i < n; i++)
@@ -19,7 +21,9 @@ int main()
         fac = fac*i;
         sum = sum + (p/fac);
     }
-    printf("%f",sum);
+    printf("The sum of the series = %f\n",sum);
 
     return 0;
 }
+
+//the sum of the series [ 1+x+x^2/2!+x^3/3!+....].
